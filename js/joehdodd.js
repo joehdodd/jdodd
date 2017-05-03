fcfbfeb0ab3a1f8b5ceefee96c6f4349fec613f1
Joe Dodd <joehdodd@gmail.com>
2017-05-03 15:47:03 -0400

    Updates

--- js/joehdodd.js
+++ js/joehdodd.js
@@ -1,29 +1,12 @@
-(function($) {
-    "use strict"; // Start of use strict
-
-    // jQuery page scrolling -- jQuery Easing plugin
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
-
-    // Closes the Navbar on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){
-            $('.navbar-toggle:visible').click();
-    });
-
-    // Floating label headings for contact form
-    $(function() {
-        $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-            $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-        }).on("focus", ".floating-label-form-group", function() {
-            $(this).addClass("floating-label-form-group-with-focus");
-        }).on("blur", ".floating-label-form-group", function() {
-            $(this).removeClass("floating-label-form-group-with-focus");
-        });
-    });
-
-})(jQuery); // End of use strict
+// jQuery page scrolling -- jQuery Easing plugin
+$('.page-scroll a').bind('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+    }, 1250, 'easeInOutExpo');
+    event.preventDefault();
+});
+// Closes the Navbar on Menu Item Click
+$('.navbar-collapse ul li a').click(function(){
+  $('.navbar-toggle:visible').click();
+});
