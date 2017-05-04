f327109c4bc3f4081340493846eba737c777a44d
Joe Dodd <joehdodd@gmail.com>
2017-05-03 20:07:08 -0400

    Update

--- js/joehdodd.js
+++ js/joehdodd.js
@@ -2,7 +2,7 @@
 $('.page-scroll a').bind('click', function(event) {
     var $anchor = $(this);
     $('html, body').stop().animate({
-        scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        scrollTop: ($($anchor.attr('href')).offset().top - 60)
     }, 1250, 'easeInOutExpo');
     event.preventDefault();
 });
